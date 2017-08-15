f64c8e7e787941fc9e90dfd812857fba9c6643eb
Peter Michaleczky <you@example.com>
2017-08-15 12:19:10 +0200

    contravariance example added

--- Covariance/Program.cs
+++ Covariance/Program.cs
@@ -13,28 +13,44 @@ namespace Covariance
             Invoice invoice1 = new Invoice("First invoice", 1);
             Invoice invoice2 = new Invoice("Second invoice", 2);
 
-            // the following line caused compile error before C# 4.0
-            BusinessDocument[] docs = { invoice1, invoice2 };
+            // this is assignment compatibility (covariance)
+            BusinessDocument[] docsarr = { invoice1, invoice2 };
+
+            // example for covariance in generics
+            IEnumerable<Invoice> invoices = new List<Invoice>() { invoice1, invoice2 };
+            IEnumerable<BusinessDocument> docs = invoices;
+
+            // contravariance example
+            invoices.ToList().Sort(new BusinessDocComparer());
+
         }
     }
 
-    class BusinessDocument
-    {        
-        string Title { get; set; }
+    class BusinessDocument {
+        public string Title { get; set; }
 
         public BusinessDocument(string title)
         {
             Title = title;
         }
+
     }
 
     class Invoice : BusinessDocument
     {
-        int InvoiceNo { get; set; }
+        public int InvoiceNo { get; set; }
 
         public Invoice(string title, int invoiceNo) : base(title)
         {
             InvoiceNo = invoiceNo;
+        }
+    }
+
+    class BusinessDocComparer : IComparer<BusinessDocument>
+    {
+        public int Compare(BusinessDocument x, BusinessDocument y)
+        {
+            return x.Title.CompareTo(y.Title);
         }
     }
 

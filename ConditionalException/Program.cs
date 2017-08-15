82b6b0115271ef7f0d11e5fa58e5053131447774
Peter Michaleczky <you@example.com>
2017-08-15 14:55:34 +0200

    ConditionalException added

---
+++ ConditionalException/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 100;
+            var divisor = 0;
+
+            try
+            {
+                var result = Divide(num, divisor);
+            }
+            catch (DivideByZeroException)
+            {
+                if (num == 100)
+                {
+                    Console.WriteLine("Divide by 0!");
+                }
+                else throw;
+            }
+
+
+            try
+            {               
+                var result = Divide(num, divisor);
+            }
+            catch (DivideByZeroException) when (num == 100)
+            {
+                Console.WriteLine("Divide by 0!");
+            }
+
+            Console.ReadKey();
+        }
+
+        static float Divide(int num, int divisor)
+        {
+            return num / divisor;
+        }
+
+    }
+}

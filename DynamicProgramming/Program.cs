b9fda00888a832fbba512a4c6f0033ac18d1d99a
Peter Michaleczky <you@example.com>
2017-08-15 14:30:32 +0200

    Dynamic example added

---
+++ DynamicProgramming/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // dynamic string
+            dynamic str = "Hello World!";
+            Console.WriteLine(str.GetType());
+            Console.WriteLine(str);
+
+            // get length of another dynamic string using reflection
+            dynamic dyn = "This is a dynamic string";
+            Console.WriteLine(GetDynamicStringLength(dyn));
+
+            // trying to get string length of a dynamic int
+            dynamic dyn2 = 42;
+            Console.WriteLine(GetDynamicStringLength(dyn2));
+
+            // exmaple on ExpandoObject
+            dynamic expando = new ExpandoObject();
+            expando.Name = "Test object";
+            expando.Id = 12;
+            Console.WriteLine($"{expando.Name}, {expando.Id}");
+
+            Console.ReadKey();
+        }
+
+        static int GetDynamicStringLength(dynamic dyn)
+        {
+            Type t = dyn.GetType();
+            if (t.Name.Equals("String")) {
+                PropertyInfo prop = t.GetProperty("Length");
+                return prop.GetValue(dyn);
+            }
+            return -1;
+        }
+    }
+}

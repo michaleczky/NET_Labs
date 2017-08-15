a93c9c37e213108dcc7f183a96806040b77d0887
Peter Michaleczky <you@example.com>
2017-08-15 10:27:39 +0200

    boxing-unboxing example

---
+++ Boxing_Unboxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // boxing
+            int num = 100;
+            object boxed_num = (object)num;
+            Console.WriteLine($"Number is {boxed_num}.");
+
+            // unboxing
+            int num_unboxed = (int)boxed_num;
+            Console.WriteLine($"Number is still {num_unboxed}.");
+
+            Console.ReadKey();
+
+        }
+    }
+}

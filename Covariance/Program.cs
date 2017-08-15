6bb61da1a814cc72acae845fcf866f3878bb59bf
Peter Michaleczky <you@example.com>
2017-08-15 11:34:14 +0200

    covariance example

---
+++ Covariance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covariance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice1 = new Invoice("First invoice", 1);
+            Invoice invoice2 = new Invoice("Second invoice", 2);
+
+            // the following line caused compile error before C# 4.0
+            BusinessDocument[] docs = { invoice1, invoice2 };
+        }
+    }
+
+    class BusinessDocument
+    {        
+        string Title { get; set; }
+
+        public BusinessDocument(string title)
+        {
+            Title = title;
+        }
+    }
+
+    class Invoice : BusinessDocument
+    {
+        int InvoiceNo { get; set; }
+
+        public Invoice(string title, int invoiceNo) : base(title)
+        {
+            InvoiceNo = invoiceNo;
+        }
+    }
+
+}

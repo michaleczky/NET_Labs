1ee70425a2b3159a2ca7821c71c4ad47b8700b79
Peter Michaleczky <you@example.com>
2017-08-21 15:26:29 +0200

    Singleton pattern example

---
+++ Singleton/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton = SingletonClass.getInstance();
+            Console.WriteLine($"Instance count: {SingletonClass.counter}");
+            var singleton2 = SingletonClass.getInstance();
+            Console.WriteLine($"Instance count: {SingletonClass.counter}");
+            var singleton3 = SingletonClass.getInstance();
+            Console.WriteLine($"Instance count: {SingletonClass.counter}");
+
+            Console.ReadKey();
+        }
+
+        // Singleton pattern implementation using Lazy instantiation and private constructor
+        class SingletonClass
+        {
+            private static readonly Lazy<SingletonClass> _instance = new Lazy<SingletonClass>(()=>new SingletonClass());
+            public static int counter = 0;
+
+            private SingletonClass()
+            {
+                // private contstructor avoids direct instantiation of the SingletonClass
+                counter++;
+            }
+
+            public static SingletonClass getInstance()
+            {
+                return _instance.Value;
+            }
+
+        }
+
+    }
+}

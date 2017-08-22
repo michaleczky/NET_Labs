877fdd54d99b890d565ba65e2a90b5a09024096d
Peter Michaleczky <you@example.com>
2017-08-22 13:12:10 +0200

    Factory pattern extended

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -10,6 +10,13 @@ namespace Factory
     {
         static void Main(string[] args)
         {
+            var invoiceFactory = new InvoiceFactory();
+            var serviceInv = invoiceFactory.CreateInvoice(InvoiceType.Services, 1);
+            var goodsInv = invoiceFactory.CreateInvoice(InvoiceType.Goods, 2);
+            Console.WriteLine($"Taxrate for service invoice is {serviceInv.TaxRate()}.");
+            Console.WriteLine($"Taxrate for goods invoice is {goodsInv.TaxRate()}.");
+
+            Console.ReadKey();
         }
     }
 

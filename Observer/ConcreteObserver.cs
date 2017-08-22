7862b92ca6aec712d01817634b920088156306cd
Peter Michaleczky <you@example.com>
2017-08-22 15:56:12 +0200

    Observer example

---
+++ Observer/ConcreteObserver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class ConcreteObserver : Observer
+    {
+        private string _name;
+        private string _observerState;
+        private ConcreteSubject _subject;
+
+        // Constructor
+        public ConcreteObserver(ConcreteSubject subject, string name)
+        {
+            this._subject = subject;
+            this._name = name;
+        }
+
+        public override void Update()
+        {
+            _observerState = _subject.SubjectState;
+            Console.WriteLine("{0}'s subject state has changed to '{1}'", _name, _observerState);
+        }
+
+        // Gets or sets subject
+        public ConcreteSubject Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+
+            subject.Attach(new ConcreteObserver(subject, "Object1"));
+            subject.Attach(new ConcreteObserver(subject, "Object2"));
+            subject.Attach(new ConcreteObserver(subject, "Object3"));
+
+            // Change subject and notify observers
+            subject.SubjectState = "NEW STATE";
+            subject.Notify();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }    
+}
